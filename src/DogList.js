6290586b0fdf85ab60a53c1bd958899e3a90d0a2
Cristian Garcia <eminemko@hotmail.com>
2021-07-31 22:55:24 -0500

    Include react bootstrap to the project, jquery and popper.

--- src/DogList.js
+++ src/DogList.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { Link } from "react-router-dom";
 
 
+
 class DogList extends Component{
 render(){
   return(
@@ -9,7 +10,7 @@ render(){
 
       {this.props.dogs.map(d => (
         <div>
-          <h1>{d.name}</h1>
+          <h1 className="display-1">{d.name}</h1>
           <img src={d.src} alt={d.name}/>
           <Link to={`/dogs/${d.name}`} > {d.name} </Link >
         </div>
