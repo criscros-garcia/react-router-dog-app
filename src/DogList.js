33ddd9e3414000024c90b39c1f49ba8e4dadd383
Cristian Garcia <eminemko@hotmail.com>
2021-08-01 00:11:39 -0500

    Start styling the images and the links

--- src/DogList.js
+++ src/DogList.js
@@ -1,22 +1,24 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
-
+import './DogList.css';
 
 
 class DogList extends Component{
 render(){
   return(
-    <div>
-
-      {this.props.dogs.map(d => (
-        <div>
-          <h1 className="display-1">{d.name}</h1>
-          <img src={d.src} alt={d.name}/>
-          <Link to={`/dogs/${d.name}`} > {d.name} </Link >
-        </div>
+    <div className='DogList'>
+      <h1 className='display-1 text-center mt-3 mb-5'>Dog List!</h1>
+      <div className='row'>
+        {this.props.dogs.map(d => (
+          <div className='Dog col-lg-4 text-center'>
+            <img src={d.src} alt={d.name}/>
+            <h3 className='mt-3'>
+            <Link className='underline' to={`/dogs/${d.name}`} > {d.name} </Link >
+            </h3>
+          </div>
       ))
-      }
-
+    }
+      </div>
     </div>
   );
   }
